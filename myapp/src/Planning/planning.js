81ac9bf0e408cd94656f67b91e45af4c5d6ac088
Archit Dadwal <83974549+architdadwal@users.noreply.github.com>
2022-07-17 23:38:30 +0530

    added basic styling yo planning page

---
+++ myapp/src/Planning/planning.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./planning.css";
+
+export default function Planning() {
+  return (
+    <div class="a">
+      <p>Please select the type of traveler you are</p>
+      <div class="b">Mountain man</div>
+      <div class="c">History lover </div>
+      <div class="d">Religious yatri </div>
+      <div class="e">The party and beach lover</div>
+    </div>
+  );
+}
