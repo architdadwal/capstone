f282971a118b47a3bba41152df1821cca6df1c8a
Archit Dadwal <83974549+architdadwal@users.noreply.github.com>
2022-07-18 23:08:26 +0530

    package error fixed

--- myapp/src/Home/Home.js
+++ myapp/src/Home/Home.js
@@ -8,7 +8,7 @@ import "./Home.css";
 const slideImages = [
   {
     url: "https://cdn.discordapp.com/attachments/947918952338706462/998111295784099871/unknown.png",
-    // caption: "Slide 1", 
+    // caption: "Slide 1",
   },
   {
     url: "https://cdn.discordapp.com/attachments/947918952338706462/998104550105960519/unknown.png",
@@ -23,26 +23,26 @@ const slideImages = [
 function Home() {
   return (
     <>
-    <div className="slide-container">
-      <Slide>
-        {slideImages.map((slideImage, index) => (
-          <div className="each-slide" key={index}>
-            <div
-              style={{
-                backgroundImage: `url(${slideImage.url})`,
-                height: "350px",
-                width: "1400px",
-                alignItems: "center",
-              }}
-            >
-              {/* <span>{slideImage.caption}</span> */}
+      <div className="slide-container">
+        <Slide>
+          {slideImages.map((slideImage, index) => (
+            <div className="each-slide" key={index}>
+              <div
+                style={{
+                  backgroundImage: `url(${slideImage.url})`,
+                  height: "350px",
+                  width: "1400px",
+                  alignItems: "center",
+                }}
+              >
+                {/* <span>{slideImage.caption}</span> */}
+              </div>
             </div>
-          </div>
-        ))}
-      </Slide>
-    </div>
-    <div>
-    {/* <Input
+          ))}
+        </Slide>
+      </div>
+      <div>
+        {/* <Input
           placeholder="🔍  Search"
           style={{
             alignContent: "center",
@@ -54,7 +54,7 @@ function Home() {
             marginLeft: "200px",
           }}
         /> */}
-        </div>
+      </div>
     </>
   );
 }
