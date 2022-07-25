da972402b5ac3b3e4d0330ee63814855dcebb836
Balram Kumar <balramkgg99@gmail.com>
2022-07-25 17:43:59 +0530

    add payment page

--- myapp/src/Activity/CityDisc.js
+++ myapp/src/Activity/CityDisc.js
@@ -1,22 +1,53 @@
-import React from 'react'
-import { Link, useLocation } from 'react-router-dom';
-
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
 function CityDisc() {
-    const location = useLocation();
-    const discription = location.state;
-const Amount =location.state;
+  const location = useLocation();
+  const discription = location.state;
+  const Amount = location.state;
   return (
-    <>
-    <img src={discription.Image} alt={discription.city}/>
-    <h1>{discription.city}</h1>
-    <p>Description: {discription.Discription}</p>
-    <h2>Amount: {Amount.Amount}</h2>
-    <h2>Contact us: xxx.yyy@gmail.com</h2>
-    <Link to={"/Booking"}>
-    <button>Book Now</button>
-    </Link>
-    </>
-  )
+    <div
+      className="citydis"
+      style={{
+        height: "500px",
+        width: "500px",
+        alighnItem: "center",
+        justifyContent: "center",
+        marginLeft: "35%",
+        borderColor: "black",
+        margin:"absolute",
+      }}
+    >
+      <img
+        src={discription.Image}
+        alt={discription.city}
+        style={{
+          alignItems: "center",
+          height: "500px",
+          width: "500px",
+          marginTop: "10px",
+        }}
+      />
+      <h1>{discription.city}</h1>
+      <p>Description: {discription.Discription}</p>
+      <h2>Amount: {Amount.Amount}</h2>
+      <h2>Contact us: xxx.yyy@gmail.com</h2>
+      <Link to={"/Booking"}  style={{alignItems:"center",
+                  display:"flex",
+                  justifyContent:"center"}} >
+        
+        <button    style={{
+                  width: "100px",
+                  height: "40px",
+                  margin: "10px",
+                  borderRadius: "5px",
+                  backgroundColor: "#007185",
+                  alignItems:"center",
+                  display:"flex",
+                  justifyContent:"center"
+                }}>Book Now</button>
+      </Link>
+      <div style={{height:"100px"}}/>
+    </div>
+  );
 }
-
-export default CityDisc
+export default CityDisc;
--- myapp/src/Activity/Payment.js
+++ myapp/src/Activity/Payment.js
@@ -1,9 +1,52 @@
-import React from 'react'
 
-function Payment() {
-  return (
-    <div>Payment</div>
-  )
-}
+import {
 
-export default Payment
+    Bookings,
+    HeadingTag,
+    PaymentContainer,
+    Text2,
+    Containers,
+    Payment,
+  } from "./Styling";
+  
+  import StripeCheckout from "react-stripe-checkout";
+ 
+ 
+  
+  function ConfirmBooking() {
+    function handleToken(token, addresses) {
+      console.log({ token, addresses });
+    }
+  
+  
+    return (
+      <Bookings>
+        <Payment>
+          <HeadingTag>
+          
+            <h1>Payment</h1>
+          </HeadingTag>
+         
+          <Containers>
+            <PaymentContainer>
+              <Text2>Card Payment</Text2>
+              {/* react-stripe-checkout is used for payment.  */}
+              <StripeCheckout
+                stripeKey="pk_test_51KuyaPSCQaVx7HBe8OxRf2sNJR6frOJUsRuVFJzPvui7Ni4icPjxO5u1YjNTWAwFtK9xxxQS03dMb6aetPKBxzSP004S3AwqfR"
+                token={handleToken}
+                billingAddress
+                shippingAddress
+                currency="inr"
+                amount={ ""}
+                name="Service Charge"
+              />
+            </PaymentContainer>
+          </Containers>
+        </Payment>
+      </Bookings>
+    );
+  }
+  
+  
+  
+  export default ConfirmBooking;
