689d12e44da64385622967b2137e956bc3a835e1
Archit Dadwal <83974549+architdadwal@users.noreply.github.com>
2022-07-25 17:50:47 +0530

    1

--- myapp/src/AboutUs/AboutUs.js
+++ myapp/src/AboutUs/AboutUs.js
@@ -1,11 +1,3 @@
 export default function AboutUs() {
-  return (
-    <div>
-      <h1>We are one stop solution for your travel cravings</h1>
-
-      <h2> Contact us </h2>
-      <h3> Phone - +919876543210</h3>
-      <h3> Email - alwaystravel@yahoo.com</h3>
-    </div>
-  );
+  return <p>Welcome to About component</p>;
 }
